c624989c824999805ad9983be3b1ec9387525b34
Danny Caperton <danny.caperton@gmail.com>
2025-08-03 11:46:03 -0600

    changed the env setup to have default values

--- src/env_setup.py
+++ src/env_setup.py
@@ -6,15 +6,10 @@ def get_log_level():
     level = os.getenv('LOG_LEVEL', 'INFO').upper()
     return getattr(logging, level, logging.INFO)
 
-def get_env_var(name):
-    value = os.getenv(name)
-    if not value:
-        raise ValueError(f"{name} not set in .env file")
-    return value
 
 load_dotenv()
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=get_log_level(), format="%(asctime)s [%(levelname)s] %(message)s")
-DB_URL = get_env_var('DATABASE_URL')
-FILE_PATH = get_env_var('FILE_PATH')
-DATASOURCE = get_env_var('DATASOURCE')
+DB_URL = os.getenv('DATABASE_URL', 'postgresql+psycopg2://postgres:password@localhost:5432/postgres')
+FILE_PATH = os.getenv('FILE_PATH', 'bucket/provider001/spx.txt')
+DATASOURCE = os.getenv('DATASOURCE', 'PROVIDER001')
